75f148f6d32dc912fdc7a7a60724f0b2b21ba6e2
sanket <sanketplus@gmail.com>
2016-01-21 15:05:58 +0530

    removed redundant code

--- parser.sh
+++ parser.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # start server with cat /tmp/f | xargs -n 1 -d '\n' ./parser.sh  2>&1 | nc -lvk 127.0.0.1 1234 > /tmp/f
 
+printfile(){
+	size=`cat $1 | wc -c`
+	echo -e "HTTP/1.1 200 OK\r\nContent-Length: $(( $size ))\r\nContent-Type: text/html\r\n\r"
+	less $1
+}
+
 requestPath=`echo $1 | awk '/GET/ {print $2}'` # get the request path
 
 	if [ "$requestPath" == "" ]; then #if the request is not a GET request	
@@ -17,24 +23,22 @@ requestPath=`echo $1 | awk '/GET/ {print $2}'` # get the request path
 				for i in `echo $dirlist` #writing dir content into a htmlfile
 				do
 					if [ "$i" == "index.html" ]; then # if dir has index.html
-						echo -e "HTTP/1.1 200 OK\r\nContent-Length: $(cat $fp/$i | wc -c)\r\nContent-Type: text/html\r\n\r"
-						cat "$fp/$i"
+						printfile "$fp/$i"						
 						exit 0
 					fi					
 					echo "<li><a href=\"$i\"/>$i</a>" >> $df
 				done
 				echo "</ul><hr></body></html>" >> $df
 				size=`cat $df | wc -c`
-				echo -e "HTTP/1.1 200 OK\r\nContent-Length: $size\r\nContent-Type: text/html\r\n\r"			
-				cat "$df"
+				#echo -e "HTTP/1.1 200 OK\r\nContent-Length: $size\r\nContent-Type: text/html\r\n\r"			
+				#cat "$df"
+				printfile $df				
 				exit 0
 
 			fi	
 		
 			if [ -f "$fp" ]; then # if requested path is file
-				size=`cat $fp | wc -c`
-				echo -e "HTTP/1.1 200 OK\r\nContent-Length: $(( $size ))\r\nContent-Type: text/html\r\n\r"
-				less $fp
+				printfile $fp
 			else # if file does not exist
 				echo -e "HTTP/1.1 404 Not Found\r\nContent-Length:30\r\nContent-Type: text/html\r\n\r\n<h1>404 Not Found</h1><br><br>"
 				exit 1
